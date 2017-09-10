f1904fe174d8f5d3192c1bcc7af0b81e0de2fdb8
uucyan <uuunouucyan@gmail.com>
2017-09-10 19:58:15 +0900

    既存メモの更新処理実装

--- app/src/main/kotlin/com/uucyan/memopush/MemoActivity.kt
+++ app/src/main/kotlin/com/uucyan/memopush/MemoActivity.kt
@@ -84,24 +84,12 @@ class MemoActivity : AppCompatActivity() {
      * アクションバーのボタンを押下した時の処理
      */
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.getItemId()
-        if (id == R.id.save_memo) {
-            // DBへの登録処理
-            Realm.getDefaultInstance().use { realm ->
-                realm.executeTransaction {
-                    val memo = realm.createObject(Memo::class.java, RealmService.idGeneration())
-
-                    // 入力値を取得
-                    val title = findViewById<EditText>(R.id.title_edit)
-                    val body = findViewById<EditText>(R.id.body_edit)
-                    val notificationTime = findViewById<TextView>(R.id.notification_time_view)
-
-                    // 登録
-//                    memo.id = RealmService.idGeneration()
-                    memo.title = title.getText().toString()
-                    memo.body = body.getText().toString()
-                    memo.notificationTime = notificationTime.getText().toString()
-                }
+        val itemId = item.getItemId()
+        if (itemId == R.id.save_memo) {
+            if (this.memoId == 0) {
+                this.createMemo()
+            } else {
+                this.updateMemo()
             }
         }
 
@@ -134,5 +122,49 @@ class MemoActivity : AppCompatActivity() {
         title.setText(memo!!.title)
         body.setText(memo.body)
         notificationTime.setText(memo.notificationTime)
+    }
+
+    /**
+     * メモの新規登録
+     * TODO: 色々とイけてないからちゃんと考えたい
+     */
+    private fun createMemo() {
+        Realm.getDefaultInstance().use { realm ->
+            realm.executeTransaction {
+                val memo = realm.createObject(Memo::class.java, RealmService.idGeneration())
+
+                // 入力値を取得
+                val title = findViewById<EditText>(R.id.title_edit)
+                val body = findViewById<EditText>(R.id.body_edit)
+                val notificationTime = findViewById<TextView>(R.id.notification_time_view)
+
+                // 登録
+                memo.title = title.getText().toString()
+                memo.body = body.getText().toString()
+                memo.notificationTime = notificationTime.getText().toString()
+            }
+        }
+    }
+
+    /**
+     * メモの更新
+     * TODO: 色々とイけてないからちゃんと考えたい
+     */
+    private fun updateMemo() {
+        Realm.getDefaultInstance().use { realm ->
+            realm.executeTransaction {
+                val memo = realm.where(Memo::class.java).equalTo("id", this.memoId).findFirst()
+
+                // 入力値を取得
+                val title = findViewById<EditText>(R.id.title_edit)
+                val body = findViewById<EditText>(R.id.body_edit)
+                val notificationTime = findViewById<TextView>(R.id.notification_time_view)
+
+                // 登録
+                memo?.title = title.getText().toString()
+                memo?.body = body.getText().toString()
+                memo?.notificationTime = notificationTime.getText().toString()
+            }
+        }
     }
 }
